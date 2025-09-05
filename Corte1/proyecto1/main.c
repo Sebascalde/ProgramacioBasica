17b00d84274fdc1ea3368adbb53dc9e870c3068d
Sebascalde <calderonsalcedo55@gmail.com>
2025-09-05 13:07:21 -0500

    Add files via upload

---
+++ Corte1/proyecto1/main.c
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*
+ run this program using the console pauser or add your own getch, system("pause") or input loop 
+*/
+
+//es que quiero aprender a comentar en c
+
+int main(int argc, char** argv) {
+	printf("hola mundo");
+	return 0;
+}
