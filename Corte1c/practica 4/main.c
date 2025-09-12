fa93a67db26ae9e7239ed558f5b0ab9d29ce88ea
Sebascalde <calderonsalcedo55@gmail.com>
2025-09-12 13:53:44 -0500

    Add files via upload

---
+++ Corte1c/practica 4/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int a,b,r;
+
+int main(int argc, char *argv[]) {
+	
+	a=8;
+	b=25;
+	r=a+b;
+	printf("El resultado de sumar %d y %d es: %d\n",a,b,r);
+	
+	if(r==40){
+		printf("el resultado %d si es =40",r);
+	}
+	else{
+		if(r < 40){
+			printf("el resultado %d no es < 40",r);
+		}
+		else{
+		printf("el resultado NO es =40",r);
+	    }
+	    
+	return 0;
+		}
+		
+		
+		
+		
+		
+	}
+	
+
