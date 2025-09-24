0842907b4b6ed5acfd36d0fbf464f4a12cd0c7f8
Sebascalde <calderonsalcedo55@gmail.com>
2025-09-23 19:36:50 -0500

    Add files via upload

---
+++ corte1c++/proyecto 7/proyecto suma2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int a,b,r;
+	a=3;
+	b=5;
+	r=a+b;
+	cout<<"resultado "<< r <<endl;
+	
+	return 0;
+}
