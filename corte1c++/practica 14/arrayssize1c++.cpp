53723c2552a3417672530ee142bd6ea3e908cec3
Sebascalde <calderonsalcedo55@gmail.com>
2025-10-09 13:27:48 -0500

    Add files via upload

---
+++ corte1c++/practica 14/arrayssize1c++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int myNumbers[] = {10, 25, 50, 75, 100};
+
+cout<<sizeof(myNumbers);
+	return 0;
+}
