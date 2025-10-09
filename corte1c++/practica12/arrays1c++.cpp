575f6e1429c277e827e16e197fdf74d55e187d87
Sebascalde <calderonsalcedo55@gmail.com>
2025-10-09 13:26:55 -0500

    Add files via upload

---
+++ corte1c++/practica12/arrays1c++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int myNumbers[] = {25, 50, 75, 100};
+cout<<myNumbers[0];
+
+	return 0;
+}
