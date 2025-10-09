6e02d834bd9e9f9199b581a33290877086cab966
Sebascalde <calderonsalcedo55@gmail.com>
2025-10-09 13:25:59 -0500

    Add files via upload

---
+++ Corte1c/practica10/arrays size 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int myNumbers[] = {10, 25, 50, 75, 100};
+int length = sizeof(myNumbers) / sizeof(myNumbers[0]);
+
+printf("%d", length);
+	return 0;
+}
