e548470e1eb296169307cccc79b845ea34189e4b
Sebascalde <calderonsalcedo55@gmail.com>
2025-10-09 13:09:57 -0500

    Add files via upload

---
+++ Corte1c/practica 8/arrays2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+int myNumbers[] = {25, 50, 75, 100};
+myNumbers[0] = 33;
+
+printf("%d", myNumbers[0]);
+
+}
