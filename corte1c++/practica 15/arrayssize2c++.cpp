284eef434bd9758b37ba7fd569f0df92e48015ac
Sebascalde <calderonsalcedo55@gmail.com>
2025-10-09 13:28:30 -0500

    Add files via upload

---
+++ corte1c++/practica 15/arrayssize2c++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int myNumbers[] = {10, 25, 50, 75, 100};
+int length = sizeof(myNumbers) / sizeof(myNumbers[0]);
+
+cout<<length; 
+	return 0;
+}
